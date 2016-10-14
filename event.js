595728435ceb05c19cb3b3ec16f7810aa54cad99
hitesh4 <saini.hitesh4@gmail.com>
2016-10-14 11:58:05 +0530

    Update Repo

---
+++ event.js
@@ -0,0 +1,18 @@
+var events = require('events');
+
+var eventEmitter = new events.EventEmitter();
+
+var connectHandler = function connected(){
+	console.log('Connection Successful');
+
+	eventEmitter.emit('data_recieved');
+}
+
+eventEmitter.on('connection', connectHandler);
+
+eventEmitter.on('data_recieved', function(){
+	console.log('daat recieved successfully');
+});
+
+eventEmitter.emit('connection');
+console.log('Program Ended');
